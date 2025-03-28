7ad31aec4245937a732505202da678e71050a3e2
Natalie <nr9601586@gmail.com>
2025-03-27 21:46:29 -0500

    Final Version

--- CardGameFinal.java
+++ CardGameFinal.java
@@ -1,12 +1,55 @@
-import java.util.ArrayList;
+/* Federico Lizarraga, Dominic Martinez, Natalie Rees
+ * CSCE 111 500
+ * 03-27-2025
+ * 
+ * RULES:
+ * Game Objective: (How do you win?)
+ * You are going to be given half a deck of shuffled cards. 
+ * The game's objective is to get all the cards from the other player.
+ * If you take all the cards you win the round. 
+ * The player who wins 2 out of 3 rounds wins (if playing the 3 round version).
+ * 
+ * This is a game between 2 players, with each player receiving 26 cards
+ * 
+ * There are 13 cards per card type(diamonds, hearts, clovers, spades), from ace to king. 
+ * The smallest value is 2, and the biggest value is A. Each card will have a numerical value. 
+ * If it's a number, its value will be the actual number. 
+ * If it's a letter T, J, Q, K, or A, its value will be 10, 11, 12, 13, and 14 (or 1), respectively.
+ * 
+ * If the cards tie, there is a war. 
+ * The players have to place three cards upside down and then flip one card, the highest card wins all cards on the table. 
+ * If the cards tie again, repeat the process. If a player runs out of cards, they will lose.
+ * 
+ * Each win will award points and the game will function on a “best out of 3” system. 
+ * In order to actually win the game, the player must win all cards twice in two separate games.
+ * 
+ * Each round, the ace will switch from being the highest ranked card (14) to the lowest ranked (1).
+ * 
+ * The players can decide if they want to play 1 round or 3 rounds via the Scanner function.
+ * 
+ * At the start of each round, both players flip over the top card of their deck. 
+ * Whoever has the higher card wins and places them at the bottom of their deck. 
+ * If the cards are the same, each player places three cards facing down, flips a fourth card to determine who the winner is, and takes all the cards on the table. 
+ * If there’s another tie, the process repeats until someone wins. The game ends when a player runs out of cards, and the other has all the cards. 
+ * To make it more interesting, the overall winner will be the one who wins 2 out of 3 rounds.
+ */
 
-public class CardGame4 {
+import java.util.ArrayList;
+import java.util.Scanner;
+public class CardGameFinal {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("How many rounds do you want to play, 1 or 3?");
+        int numbergames = scnr.nextInt();
         int wins1 = 0;
         int wins2 = 0;
         int gameNumber = 1;
+        while(numbergames!=1 && numbergames!=3) {
+            System.out.println("Please a choose a number between 1 and 3: ");
+            numbergames = scnr.nextInt();
+        }
 
-        while (wins1 < 2 && wins2 < 2) { // Best of 3 (First to 2 wins)
+        while (wins1 < numbergames && wins2 < numbergames) { // Best of 3 (First to 2 wins)
             System.out.println(""); // Formatting
             System.out.println("Starting Game " + gameNumber);
 
@@ -22,36 +65,64 @@ public class CardGame4 {
 
             // Determine the winner of this game
             if (deck1.isEmpty()||deck2.size()>deck1.size()) {
+                if(numbergames==1){
+                    System.out.println(""); //Formatting, can be changed
+                System.out.println("=============================");
+                System.out.println("Player 2 wins Game ");
+                System.out.println("Thank you for playing!");
+                System.out.println("=============================");
+                System.out.println("");
+                wins2++;
+                }
+                else{
                 System.out.println(""); //Formatting, can be changed
                 System.out.println("=============================");
                 System.out.println("Player 2 wins Game " + gameNumber + "!");
                 System.out.println("=============================");
                 System.out.println("");
                 wins2++;
+                }
             } else {
-                System.out.println("");
+                if(numbergames==1){
+                    System.out.println(""); //Formatting, can be changed
                 System.out.println("=============================");
-                System.out.println("Player 1 wins Game " + gameNumber + "!");
+                System.out.println("Player 1 wins Game " );
+                System.out.println("Thank you for playing!");
                 System.out.println("=============================");
                 System.out.println("");
                 wins1++;
+                }
+                else{
+                System.out.println("");
+                System.out.println("=============================");
+                System.out.println("Player 1 wins Game " + gameNumber + "!");
+                System.out.println("Thank you for playing!");
+                System.out.println("=============================");
+                System.out.println("");
+                wins1++;
+                }
             }
             System.out.println("That was " + totalRounds + " rounds!");
             gameNumber++;
         }
 
         // Announce the overall winner
+        if (numbergames == 3) {
+            
         System.out.println("");
         System.out.println("=============================");
         System.out.println("");
         if (wins1 > wins2) {
             System.out.println("Player 1 wins the Best of 3 Series!");
+            System.out.println("Thank you for playing!");
         } 
         if(wins2>wins1) {
             System.out.println("Player 2 wins the Best of 3 Series!");
+            System.out.println("Thank you for playing!");
         }
         System.out.println("");
         System.out.println("=============================");
+        }
     }
 
     // Method to create and shuffle a deck without Collections
@@ -97,11 +168,13 @@ public class CardGame4 {
                 System.out.println("");
                 deck1.add(card1);
                 deck1.add(card2);
+                toggleAce();
             } else if (value2 > value1) {
                 System.out.println("Player 2 wins this round.");
                 System.out.println("");
                 deck2.add(card1);
                 deck2.add(card2);
+                toggleAce();
             } else { // War scenario
                 System.out.println("It's a WAR!");
 
@@ -168,7 +241,6 @@ public class CardGame4 {
         }
         return rounds;
     }
-
     private static boolean aceIsHighest = true;
 
     // Changed to switch for conciseness (Was If Chain)
